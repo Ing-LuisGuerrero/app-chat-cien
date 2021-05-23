e8c367222481ec96e43111364710d358c7364bb3
Ing-LuisGuerrero <ing.luis_guerrero@hotmail.com>
2021-05-23 00:55:00 -0500

    Send messages done

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,47 +1,75 @@
 <template>
-  <li class="flex">
-    <div class="ml-3 flex justify-end">
+  <li v-if="didISendIt" class="mt-3 flex justify-end">
+    <div class="ml-3 flex justify-end w-full">
       <p
         class="rounded-xl bg-[#0099ff] px-4 py-2 lg:max-w-[70%] text-sm text-white"
       >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis,
-        magnam necessitatibus mollitia velit alias nobis illo ea, dicta ipsa
-        aliquam, adipisci saepe labore accusamus? Doloremque sunt facilis
-        excepturi labore eligendi.
+        {{ message.message }}
       </p>
     </div>
   </li>
-  <li class="flex mb-2">
+  <li v-else class="flex mt-3">
     <div class="flex-shrink-0 self-end">
       <img
         class="flex-grow-0 flex-shrink-0 rounded-full h-7 w-7 object-cover"
         src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
       />
     </div>
-    <div class="ml-3">
-      <p class="rounded-xl bg-gray-200 px-4 py-2 lg:max-w-[70%] text-sm">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis,
-        magnam necessitatibus mollitia velit alias nobis illo ea, dicta ipsa
-        aliquam, adipisci saepe labore accusamus? Doloremque sunt facilis
-        excepturi labore eligendi.
+    <div class="ml-3 w-full">
+      <p class="rounded-xl bg-gray-200 px-4 py-2 w-min lg:max-w-[70%] text-sm">
+        {{ message.message }}
       </p>
     </div>
   </li>
 </template>
 
 <script>
+import { ref } from "@vue/reactivity";
 export default {
   props: {
     message: {
       type: Object,
       required: true,
     },
-    user: {
-      type: String,
+    didISendIt: {
+      type: Boolean,
       required: true,
     },
   },
 
-  setup() {},
+  setup(props) {
+    const token = localStorage.getItem("token");
+    const id = localStorage.getItem("id");
+    const user = ref({});
+
+    const getUser = async () => {
+      const res = await fetch(
+        `http://localhost:4000/api/user/${props.message.user}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Access-Token": token,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      user.value = data;
+      console.log("enter");
+      console.log(user.value);
+    };
+
+    console.log(`${props.message.user} === ${id}`);
+    console.log(props.message.user !== id);
+    if (props.message.user !== id) {
+      getUser();
+    }
+
+    return {
+      user,
+    };
+  },
 };
 </script>
--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -7,7 +7,7 @@
       src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
     />
     <div class="ml-3 flex-1 h-full flex flex-col justify-center">
-      <p class="text-base font-bold">Someone Unknown</p>
+      <p class="text-base font-bold">{{ chatName }}</p>
       <p class="text-xs text-gray-500">Activo hace 38 min</p>
     </div>
     <!-- Actions -->
@@ -20,6 +20,11 @@
   <div class="flex-1 flex flex-col">
     <ul class="flex-1 flex flex-col-reverse pl-1 pr-6 pb-1">
       <!-- Display messages here -->
+      <Message
+        v-for="message in messages"
+        :message="message"
+        :didISendIt="didISendIt(message)"
+      />
     </ul>
     <div class="flex items-center py-2 px-3 w-full">
       <button
@@ -32,10 +37,12 @@
           type="text"
           placeholder="Aa"
           class="w-full rounded-full border-none outline-none py-2 px-3 bg-gray-100"
+          v-model="newMessage.message"
         />
       </label>
       <button
         class="ml-2 hover:bg-gray-100 rounded-full px-2 py-2.5 flex justify-center items-center"
+        @click="sendMessage()"
       >
         <PaperAirplaneIcon
           class="transform rotate-90 h-5 w-5 text-blue-500 block ml-1"
@@ -52,13 +59,116 @@ import {
   PhotographIcon,
   PaperAirplaneIcon,
 } from "@heroicons/vue/solid";
+
+import { useRoute } from "vue-router";
+import { ref, reactive } from "@vue/reactivity";
+import Message from "../components/Message.vue";
+
 export default {
   components: {
     InformationCircleIcon,
     PhotographIcon,
     PaperAirplaneIcon,
+    Message,
   },
 
-  setup() {},
+  setup(props) {
+    const router = useRoute();
+    const token = localStorage.getItem("token");
+    const user = localStorage.getItem("id");
+    const chatId = router.params.id;
+    const chat = reactive({});
+    const messages = ref([]);
+    const chatName = ref("");
+    const newMessage = reactive({
+      message: "",
+      user,
+      chat: "",
+    });
+
+    const getChat = async () => {
+      const res = await fetch(`http://localhost:4000/api/chats/${chatId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Access-Token": token,
+        },
+      });
+
+      const data = await res.json();
+
+      chat.value = data;
+      chatName.value = getChatName(chat.value);
+      newMessage.chat = chat.value._id;
+    };
+
+    const getMessages = async () => {
+      const res = await fetch(
+        `http://localhost:4000/api/messages/chat/${chatId}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Access-Token": token,
+          },
+        }
+      );
+
+      const data = await res.json();
+
+      messages.value = data;
+    };
+
+    getChat();
+    getMessages();
+
+    const didISendIt = (message) => {
+      console.log(message);
+      return message.user === user;
+    };
+
+    const getChatName = (chat) => {
+      if (chat.users.length > 2) {
+        let name = "";
+        for (let i = 0; i < chat.users.length; i++) {
+          if (chat.users[i].id !== user) {
+            name += chat.users[i].name.split()[0];
+          }
+        }
+        return name;
+      } else {
+        for (let i = 0; i < chat.users.length; i++) {
+          if (chat.users[i]._id !== user) {
+            return chat.users[i].name;
+          }
+        }
+      }
+    };
+
+    const sendMessage = async () => {
+      console.log(newMessage);
+      const res = await fetch(`http://localhost:4000/api/messages/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Access-Token": token,
+        },
+        body: JSON.stringify(newMessage),
+      });
+
+      const data = await res.json();
+      const response = data;
+      console.log(response);
+    };
+
+    return {
+      chat,
+      didISendIt,
+      chatName,
+      messages,
+      newMessage,
+      sendMessage,
+    };
+  },
 };
 </script>
